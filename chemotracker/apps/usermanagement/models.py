c79bb928b132eecf37d2b751a938306bd81f03a3
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2017-05-13 21:24:20 -0400

    Using django rest framework

--- chemotracker/apps/usermanagement/models.py
+++ chemotracker/apps/usermanagement/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-from django.utils import timezone
-
-class User (models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
-    age = models.PositiveIntegerField()
-    sex = models.PositiveSmallIntegerField()
-    date_joined = models.DateTimeField(default=timezone.now)
