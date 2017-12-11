ae8e19f1b52ac5bfbf7006aa3b80529bbd085405
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2017-12-10 19:17:55 -0500

    Removing old models

--- chemotracker/usermanagement/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-class IsTargetUser(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # allow logged in user to view own details
-        return obj == request.user
