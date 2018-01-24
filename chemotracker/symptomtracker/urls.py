53294b0a2d619619f47db0d27233c05262618420
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2018-01-24 14:29:34 -0500

    Changing get url

--- chemotracker/symptomtracker/urls.py
+++ chemotracker/symptomtracker/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views	
 
 urlpatterns = [
-    url(r"^$", views.symptoms, name="get_symptoms"),
+    url(r"^symptoms/$", views.symptoms, name="get_symptoms"),
 ]
