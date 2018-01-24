1fbb3ae480bad29a034604f69c5b69db62f42f91
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2018-01-24 13:12:35 -0500

    Setting email backend

--- chemotracker/chemotracker/settings.py
+++ chemotracker/chemotracker/settings.py
@@ -150,4 +150,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Authentication Configs
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend"
+)
+
+ACCOUNT_AUTHENTICATION_METHOD='username_email'
 ACCOUNT_EMAIL_REQUIRED=True
