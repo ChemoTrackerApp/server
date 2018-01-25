3c927f1b64c712a76b4c288a76c8bc37582817fc
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2018-01-24 22:14:43 -0500

    Loaded data for grades and added request

--- chemotracker/symptomtracker/models.py
+++ chemotracker/symptomtracker/models.py
@@ -16,3 +16,37 @@ class Symptom(models.Model):
             "id": self.id,
             "name": self.name
         }
+
+class Grade(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = _("grade")
+        verbose_name_plural = _("grades")
+
+    def __str__(self):
+        return "%s: %s" % (self.symptom.name, self.name)
+
+
+class SymptomGrade(models.Model):
+    symptom = models.ForeignKey('Symptom', on_delete=models.CASCADE)
+    description = models.TextField()
+    grading_patient_friendly = models.TextField()
+    intervention_patient_friendly = models.TextField()
+    grade = models.ForeignKey('Grade', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("symptom grade description")
+        verbose_name_plural = _("symptom grade descriptions")
+
+    def __str__(self):
+        return "%s: %s" % (self.symptom.name, self.name)
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "name": self.grade.name,
+            "description": self.description,
+            "patientFriendlyDescription": self.grading_patient_friendly,
+            "patientFriendlyIntervention": self.intervention_patient_friendly
+        }
--- chemotracker/symptomtracker/views.py
+++ chemotracker/symptomtracker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
-from symptomtracker.models import Symptom
+from symptomtracker.models import Symptom, SymptomGrade
 import json
 
 @require_http_methods(["GET"])
@@ -9,3 +9,21 @@ def symptoms(request):
     symptoms = Symptom.objects.all()
     response = [ obj.as_dict() for obj in symptoms ]
     return HttpResponse(json.dumps({"data": response}), content_type='application/json')
+
+@require_http_methods(["GET"])
+def grades(request):
+    symptom_name = request.GET.get('symptom')
+    if symptom_name is None:
+        return HttpResponseBadRequest('Need to specify symptom as URL Parameter')
+
+    symptom = Symptom.objects.get(name=symptom_name)
+
+    if symptom is None:
+        return HttpResponseBadRequest('Symptom not found!')
+
+    grades = SymptomGrade.objects.filter(symptom=symptom)
+
+    response = [ obj.as_dict() for obj in grades ]
+
+    return HttpResponse(json.dumps({symptom.name: response}), content_type='application/json')
+
