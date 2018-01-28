bf64936fd8e934a13d452016012a08e041c4d166
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2018-01-27 20:04:06 -0500

    Adding search query

--- chemotracker/patientprofile/urls.py
+++ chemotracker/patientprofile/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views	
 
 urlpatterns = [
-    url(r"profile", views.profile, name="profile"),
+    url(r"", views.users, name="users"),
+    url(r"profile", views.profile, name="profile")
 ]
