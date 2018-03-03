f530a2413827e0868eff0f1feeb8f4ca47fe1abd
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2018-03-03 13:15:47 -0500

    Fixing urls

--- chemotracker/patientprofile/urls.py
+++ chemotracker/patientprofile/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views	
 
 urlpatterns = [
-    url(r"", views.users, name="users"),
+    url(r"^search/", views.users, name="users"),
     url(r"^profile/$", views.profile, name="profile")
 ]
