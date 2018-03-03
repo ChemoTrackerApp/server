15aeb1d0c5e695b77a1a18e28373ad3c05c4e9a6
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2018-03-02 21:30:13 -0500

    More profile progress and started work on events

--- chemotracker/events/models.py
+++ chemotracker/events/models.py
@@ -2,4 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+class Event(models.Model):
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	start = models.DateTimeField()
+	end = models.DateTimeField()
+	creation_date = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=256)
+	notes = models.TextField()
 
+	class Meta:
+        verbose_name = _("event")
+        verbose_name_plural = _("events")
+
+    def __str__(self):
+        return "%s" % (self.title)
+
+	def as_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "start": self.start.strftime('%Y-%m-%d %H:%M:%S'),
+            "end": self.end.strftime('%Y-%m-%d %H:%M:%S'),
+            "notes": self.notes,
+            "creation_date": self.creation_date.strftime('%Y-%m-%d %H:%M:%S')
+        }
--- chemotracker/events/views.py
+++ chemotracker/events/views.py
@@ -1,3 +1,68 @@
+from django.contrib.auth.models import User
+from django.db.models import Q
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+from django.views.decorators.http import require_http_methods
+from events.models import Event
+import time
+import datetime
+import json
 
-# Create your views here.
+
+@require_http_methods(["GET", "POST"])
+def event(request):
+	if request.method == 'GET':
+        return get_event(request)
+    elif request.method == 'POST':
+        return create_event(request)
+
+def get_event(request):
+	if request.user is None or not request.user.is_authenticated:
+		return HttpResponseForbidden("Missing or invalid Authorization token")
+
+	year = request.GET.get('year')
+    month = request.GET.get('month')
+    day = request.GET.get('day')
+
+    if year is None or month is None or day is None:
+        return HttpResponseBadRequest("Missing year or month or day field")
+
+    try:
+        query_date = datetime.date(int(year), int(month), int(day))
+    except ValueError:
+        return HttpResponseBadRequest("Invalid date")
+    except Exception:
+        return HttpResponseBadRequest("Invalid date. Parameters must be invalid.")
+
+
+	events = Event.objects.filter(Q(start__date=query_date) | Q(end__date=query_date) )
+
+	response = [ obj.as_dict() for obj in events ]
+
+	HttpResponse(json.dumps({"events": response}), content_type='application/json')
+
+def create_event(request):
+	if request.user is None or not request.user.is_authenticated:
+		return HttpResponseForbidden("Missing or invalid Authorization token")
+
+	data = JSONParser().parse(request)
+
+	start_year = data['startYear']
+	start_month = data['startMonth']
+	start_day = data['startDay']
+	# Need start time
+	end_year = data['endYear']
+	end_month = data['endMonth']
+	end_day = data['endDay']
+	# Need end time
+
+	try:
+        start_date = datetime.date(int(start_year), int(start_month), int(start_day))
+    except ValueError:
+        return HttpResponseBadRequest("Invalid date")
+    except Exception:
+        return HttpResponseBadRequest("Invalid date. Parameters must be invalid.")
+
+
+	title = data['title']
+
