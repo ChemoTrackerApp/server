cd72e65dc9e4331ef4523bff62d3082e177fdc34
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2018-03-13 16:19:43 -0400

    url error

--- chemotracker/patientprofile/urls.py
+++ chemotracker/patientprofile/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r"^search/", views.users, name="users"),
-    url(r"^profile/$", views.profile, name="profile")
-    url(r"^s3-info/", views.get_s3_info, name="s3-info"),
+    url(r"^profile/$", views.profile, name="profile"),
+    url(r"^s3-info/", views.get_s3_info, name="s3-info")
 ]
