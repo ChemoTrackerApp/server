29c462c66de8dbd45f9440dc6062e9b614f92507
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2018-03-13 14:33:25 -0400

    Require fewer fields

--- chemotracker/chemotracker/settings.py
+++ chemotracker/chemotracker/settings.py
@@ -160,9 +160,10 @@ AUTHENTICATION_BACKENDS = (
     # `allauth` specific authentication methods, such as login by e-mail
     "allauth.account.auth_backends.AuthenticationBackend"
 )
-
+    
 ACCOUNT_AUTHENTICATION_METHOD='email'
 ACCOUNT_EMAIL_REQUIRED=True
+ACCOUNT_USERNAME_REQUIRED = False
 REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'patientprofile.serializers.RegisterSerializer'
 }
